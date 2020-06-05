3d846c5cc44befd8421cb7d43e8db638122e9ec2
unknown <dariuscapolna@yahoo.com>
2020-06-05 11:18:16 +0300

    Added touch events

--- input.js
+++ input.js
@@ -8,6 +8,68 @@ let lastInput = {
     y: 0
 }
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            if (lastInput.x !== 0) return
+            inputDirection = {
+                x: -1,
+                y: 0
+            }
+        } else {
+            if (lastInput.x !== 0) return
+            inputDirection = {
+                x: 1,
+                y: 0
+            }
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            if (lastInput.y !== 0) return
+            inputDirection = {
+                x: 0,
+                y: -1
+            }
+        } else { 
+            if (lastInput.y !== 0) return
+            inputDirection = {
+                x: 0,
+                y: 1
+            }
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
 window.addEventListener("keydown", e => {
     switch (e.key) {
         case "ArrowUp":
